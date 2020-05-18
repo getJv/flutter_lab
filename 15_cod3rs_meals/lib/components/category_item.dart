09ea72b8abbf6b6938a8d638afda5f62ea76d7a5
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-05-18 09:23:50 -0300

    up

---
+++ 15_cod3rs_meals/lib/components/category_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/utils/app_routes.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem({Key key, this.category}) : super(key: key);
+
+  void _selectedcategory(BuildContext context) {
+    Navigator.of(context)
+        .pushNamed(AppRoutes.CATEGORY_MEALS, arguments: category);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => _selectedcategory(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+      ),
+    );
+  }
+}
